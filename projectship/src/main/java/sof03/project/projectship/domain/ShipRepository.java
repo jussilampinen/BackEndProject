098df766c94b499c493b09d088b7c3918c257c45
JLampinan <jussitussi16@gmail.com>
2025-04-13 18:52:59 +0300

    Added some tests

--- projectship/src/main/java/sof03/project/projectship/domain/ShipRepository.java
+++ projectship/src/main/java/sof03/project/projectship/domain/ShipRepository.java
@@ -1,7 +1,9 @@
 package sof03.project.projectship.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ShipRepository extends CrudRepository<Ship, Long> {
-
+    List<Ship> findByShipName(String shipName);
 }
--- projectship/src/test/java/sof03/project/projectship/ProjectshipApplicationTests.java
+++ projectship/src/test/java/sof03/project/projectship/ProjectshipApplicationTests.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import sof03.project.projectship.domain.Ship;
+import sof03.project.projectship.domain.ShipRepository;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -16,6 +20,9 @@ class ProjectshipApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
+
+	@Autowired
+	private ShipRepository shipRepository;
 
     @Test
     void contextLoads() {
@@ -29,5 +36,52 @@ class ProjectshipApplicationTests {
                 .andExpect(status().isOk())
                 .andExpect(view().name("shiplist"))
                 .andExpect(model().attributeExists("ships"));
+    }
+
+	@Test
+    void loginPageIsAvailable() throws Exception {
+        mockMvc.perform(get("/login"))
+               .andExpect(status().isOk())
+               .andExpect(view().name("login")); // make sure the login view name is correct
+    }
+
+	@Test
+    void accessShiplistWithoutLoginRedirects() throws Exception {
+        mockMvc.perform(get("/shiplist"))
+               .andExpect(status().is3xxRedirection())
+               .andExpect(redirectedUrlPattern("**/login"));
+    }
+
+	@Test
+    @WithMockUser(username = "user", roles = { "USER" })
+    void accessShiplistWithUserRole() throws Exception {
+        mockMvc.perform(get("/shiplist"))
+               .andExpect(status().isOk());
+    }
+
+	@Test
+    @WithMockUser(username = "admin", roles = { "USER" })
+    void accessAdminPageWithWrongRoleShouldFail() throws Exception {
+        mockMvc.perform(get("/admin"))
+               .andExpect(status().isForbidden());
+    }
+
+	@Test
+    public void whenFindByShipName_thenShipShouldBeFound() {
+        
+        Ship titanic = new Ship("Titanic", 52310, 269.1, 28.2, 1912, null, null, null, null, null);
+        shipRepository.save(titanic);
+
+        Ship found = shipRepository.findByShipName("Titanic").get(0);
+
+        assertThat(found).isNotNull();
+        assertThat(found.getShipName()).isEqualTo("Titanic");
+    }
+
+
+	@Test
+    void titanicIsPresentWithCorrectFate() {
+        Ship titanic = shipRepository.findByShipName("Titanic").get(0);
+        assertThat(titanic.getFate().getDescription()).contains("Sank during maiden voyage");
     }
 }
